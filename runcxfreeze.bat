570d4b4b7503c2c02298e5750bd4961e102001fb
Noel O'Boyle <baoilleach@gmail.com>
2018-01-01 12:55:07 +0000

    Update script for creating cx_freeze distribution for running on Win10 with latest cxfreeze.

--- runcxfreeze.bat
+++ runcxfreeze.bat
@@ -1,5 +1,5 @@
 @echo off
 cd src
 rem "python setup.py build" will just make the build folder if desired
-python setup.py bdist_msi
+py setup.py bdist_msi
 cd ..
--- src/setup.py
+++ src/setup.py
@@ -9,16 +9,25 @@ docfiles = glob.glob(os.path.join("..", "docs", "*.html"))
 docfiles_dest = [x.replace("..\\docs", "Docs") for x in docfiles]
 assert len(docfiles) > 0
 
+import os.path
+PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
+os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
+os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
+
 setup(name="GaussSum",
       version=version,
       author="Noel O'Boyle",
       author_email="baoilleach@users.sf.net",
       url="http://gausssum.sf.net",
-      executables = [Executable('GaussSum.py', base="Win32GUI", shortcutName="GaussSum", shortcutDir = "ProgramMenuFolder")],
+      executables = [Executable('GaussSum.py', base="Win32GUI", icon="../logo/GaussSum.ico", shortcutName="GaussSum", shortcutDir = "ProgramMenuFolder")],
       options = {"build_exe" : {
-          "icon" : "../logo/GaussSum.ico",
-          "includes": ["gausssum"],
-          "include_files" : [('gausssum/mesh.gif', 'mesh.gif'), ('gausssum/mesh2.gif', 'mesh2.gif')] + list(zip(docfiles, docfiles_dest)),
+          "includes": ["gausssum",
+                       "numpy.core._methods", "numpy.lib.format"], # Missing at runtime
+          "include_files" : [
+            os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tk86t.dll'),
+            os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tcl86t.dll'),
+            ('gausssum/mesh.gif', 'mesh.gif'),
+            ('gausssum/mesh2.gif', 'mesh2.gif')] + list(zip(docfiles, docfiles_dest)),
           "packages":["six"], # Workaround to avoid error message
           }},
       )
